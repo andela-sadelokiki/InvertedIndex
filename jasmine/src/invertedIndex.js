26b87b8c25f1689f8c82ad846b49289b7d85a2be
Susan <susan.adelokiki@andela.co>
2015-08-31 15:13:03 +0100

    Create getIndex and Search functions

--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -4,28 +4,44 @@ var data = [{
     },
 
     {
-        "title": "The Lord of the Rings: The Fellowship of the Ring.",
+        "title": "The Alice Lord of the Rings: The Fellowship of the Ring.",
         "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
     }
 ];
 var arr = [];
 var dict = {}
-for (var i in data) {
-    for(var key in data[i]) {
-        var words = data[i][key].split(" ");
-        for(var j in words) {
-            var word = words[j];
-            if(dict.hasOwnProperty(word)) {
-                var posArr = dict[word];
-                if(parseInt(i)) {
-                posArr.push(parseInt(i));
-                dict[word] = posArr
+
+var getIndex = function() {
+    for (var i in data) {
+        for (var key in data[i]) {
+            var words = data[i][key].split(" ");
+            for (var j in words) {
+                var word = words[j].replace(/[:,.]/g, '') 
+                if (dict.hasOwnProperty(word)) {
+                    var posArr = dict[word];
+                    if (parseInt(i)) {
+                        var position = parseInt(i)
+                        if (posArr.indexOf(position) < 0) {
+                            posArr.push(parseInt(i));
+                            dict[word] = posArr
+                        }
+                    }
+                } else {
+                    dict[word] = [parseInt(i)];
                 }
-            }
-            else {
-                dict[word] = [parseInt(i)];
             }
         }
     }
+    console.log(dict);
+    return dict
 }
-dict;
+
+getIndex();
+
+var searchIndex = function(terms) {
+    var check = {};
+    check[terms] = getIndex()[terms]
+    console.log(check);
+    return check;
+}
+searchIndex("man");
