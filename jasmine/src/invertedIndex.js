ec35e0556cb8fa8190ff703a4bb657957e0e7cf1
Susan <susan.adelokiki@andela.co>
2015-09-23 20:12:53 +0100

    Reduce number of loops Improve on comments Improve createIndex function

--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -1,111 +1,75 @@
 'use strict';
 
+var Index = function() {
+  this.file;
+  this.index;
+};
+
 /* function readJson loads the books.json file in a synchronous way because the data is needed
  * during page load.
  */
-function readJson(filePath) {
-    var json;
-    $.ajax({
-        'async': false,
-        'url': filePath,
-        'dataType': 'json',
-        'success': function(data) {
-            json = data;
-        }
-    });
-    return json;
+function loadJson(filePath) {
+  var jsonDocument;
+  $.ajax({
+    'async': false,
+    'url': filePath,
+    'dataType': 'json',
+    'success': function(data) {
+      jsonDocument = data;
+    }
+  });
+  return jsonDocument;
 }
-//Assign data from 'books.json' to jsonArray
-var jsonArray = readJson('books.json');
 
-//Create index constructor
-var Index = function() {};
-
-//Create createIndex prototype of Index constructor
+//This method creates an Index from the argument, and assigns it to an instance variable
 Index.prototype.createIndex = function(filePath) {
-    //Object "bookIndex" is initialized, this is where the index will be.
-    var bookIndex = {};
-    //the JSON array gotten from the filepath is stored in json
-    var json = readJson(filePath);
-    //Loop through the json array to get the elements
-    for (var i = 0; i < json.length; i++) {
-        //loop through the array elements and assign each to 'jsonObject'
-        var jsonObject = json[i];
-        for (var key in jsonObject) {
-            //the elements are split into 'text' and 'titles' and stored in words as arrays
-            var words = jsonObject[key].split(' ');
-            for (var j = 0; j < words.length; j++) {
-                /*loop through the titles and texts, gets each word and assigns to word
-                 *regular expression is also used to remove commas, colons and fullstops.
-                 */
-                var word = words[j].replace(/[:,.]/g, '');
-                //Check if Object bookIndex has key, word
-                if (bookIndex.hasOwnProperty(word)) {
-                    /*if it does, we push the value which is the array position in array
-                     *called positionArray
-                     */
-                    var positionArray = bookIndex[word];
-                    //check if the index exists
-                    if (parseInt(i)) {
-                        //assign indexes to position
-                        var position = parseInt(i);
-                        //check if the position already does not exist in positionArray, push to positionArray
-                        if (positionArray.indexOf(position) < 0) {
-                            positionArray.push(position);
-                            /*assign bookIndex[word] as keys and positionArray as corresponding values of object
-                             *bookIndex
-                             */
-                            bookIndex[word] = positionArray;
-                        }
-                    }
-                    /*else if position exists,assign bookIndex[word] as keys and [parseInt(i)] as
-                     *corresponding values of object bookIndex
-                     */
-                } else {
-                    bookIndex[word] = [parseInt(i)];
-                }
-            }
+  this.file = filePath;
+  var jsonFile = loadJson(filePath),
+    result = {};
+  jsonFile.forEach(function(element, index) {
+    //This removes 'text', 'title', and other characters from json object
+    var jsonString = JSON.stringify(element)
+      .replace(/,(?=\s)|[:.{}""]|\btitle\b|\btext\b/g, '')
+      .replace(/,(?=\S)/g, ' ')
+      .split(' ');
+    for (var i = 0; i < jsonString.length; i++) {
+      if (result.hasOwnProperty(jsonString[i])) {
+        var positionArray = result[jsonString[i]];
+        if (positionArray.indexOf(index) < 0) {
+          positionArray.push(index);
+          result[jsonString[i]] = positionArray;
         }
+      } else {
+        result[jsonString[i]] = [index];
+      }
     }
-    /*return object "bookIndex", containing the words and corresponding
-     *index when createIndex method is called
-     */
-    return bookIndex;
+  });
+  this.index = result;
 };
 
-//Create new instance of Index constructor
-var index1 = new Index();
+//This method returns an object that is an accurate index of the content of the JSON file.
+Index.prototype.getIndex = function(jsonPath) {
+  if (jsonPath === this.file) {
+    return this.index;
+  }
+};
 
-//Call getIndex method on new instance on Index constructor
-var getIndex = index1.createIndex;
-
-//Filepath is passed in to get the bookList of words in the json file.
-var bookList = getIndex('books.json');
-
-//Create searchIndex prototype of Index constructor
+//This method returns an array of numbers, representing the index of an object in the JSON file.
 Index.prototype.searchIndex = function(terms) {
-    //Array "check" is initialized, this is where the search results will be.
-    var check = [];
-    // assign arguments to variable params
-    var params = arguments;
-
-    //check for multiple terms
-    if (typeof terms === 'object') {
-        params = terms;
-    }
-    /*Loop through booklist to check if the terms exist, then push the corresponding index
-    into an array*/
-    for (var word in bookList) {
-        for (var j = 0; j < params.length; j++) {
-            if (word === params[j]) {
-                for (var k in bookList[word]) {
-                    check.push(parseInt(bookList[word][k]));
-                }
-            }
+  var result = [],
+    //Using arguments as variable name throws error in strict mode
+    params = arguments;
+  if (typeof terms === 'object') {
+    params = terms;
+  }
+  for (var word in this.index) {
+    for (var j = 0; j < params.length; j++) {
+      if (word === params[j]) {
+        for (var k = 0; k < this.index[word].length; k++) {
+          result.push(parseInt(this.index[word][k]));
         }
+      }
     }
-    //return the array check when the function is called
-    return check;
+  }
+  return result;
 };
-
-var searchResult = Index.prototype.searchIndex;
